5e54928e64de24952d4093fb29cfc8e25c5b54fb
TON SANTOS <61169998+TONSANTOS@users.noreply.github.com>
2025-04-21 03:08:53 -0300

    fix: Ajuste no caminho do componente NavBar na importação dentro do componente App

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ContactForm } from "./components/ContactForm";
 import { Education } from "./components/Education";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./components/Navbar";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { WorkExperience } from "./components/WorkExperience";
